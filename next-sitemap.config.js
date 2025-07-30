a8467f6309c83a935bb815c0dc464003191296ed
Aman Jaiswal <aj007.12@gmail.com>
2025-07-31 04:24:48 +0530

    Add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+export default {
+    siteUrl: 'https://wecodeman.com', // Replace with your actual domain
+    generateRobotsTxt: true, // (optional) Generate robots.txt
+    sitemapSize: 5000,        // (optional) limit of entries per sitemap file
+  };
